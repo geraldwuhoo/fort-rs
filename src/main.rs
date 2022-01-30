f1329d303dfcd16097da440af56485b06a6a6a3f
98WuG <gerald@geraldwu.com>
2022-01-29 19:08:40 -0800

    Refactor to abstract out PasswordGenerator logic to enum. Add Sha3-512.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,67 @@ use anyhow::Result;
 use hmac::{Hmac, Mac, NewMac};
 use scrypt;
 use sha2::Sha512;
+use sha3::Sha3_512;
 use std::io;
-use log::debug;
+
+pub enum PasswordGeneratorAlgo {
+    Sha512,
+    Sha3,
+}
+
+pub enum PasswordGenerator {
+    HmacSha512(Hmac<Sha512>),
+    HmacSha3(Hmac<Sha3_512>),
+}
+
+impl PasswordGenerator {
+    pub fn new(algo: PasswordGeneratorAlgo, key: &str) -> Result<Self> {
+        // Create master key for Hmac
+        let mut master_key: [u8; 64] = [0; 64];
+        let (log_n, r, p) = (15, 8, 2);
+        let params = scrypt::Params::new(log_n, r, p)?;
+        scrypt::scrypt(key.as_bytes(), b"", &params, &mut master_key)?;
+
+        // Create desired Hmac from master key
+        match algo {
+            PasswordGeneratorAlgo::Sha512 => Ok(Self::HmacSha512(
+                Hmac::<Sha512>::new_from_slice(&master_key).expect("HMAC accepts any key length"),
+            )),
+            PasswordGeneratorAlgo::Sha3 => Ok(Self::HmacSha3(
+                Hmac::<Sha3_512>::new_from_slice(&master_key).expect("HMAC accepts any key length"),
+            )),
+        }
+    }
+
+    pub fn create_site_password(&mut self, seed: &str, pass_chars: &Vec<char>) -> String {
+        // Define pattern matching macro
+        macro_rules! match_hmac {
+            ($value:expr, $pattern:pat => $result:expr) => {
+                match $value {
+                    Self::HmacSha512($pattern) => $result,
+                    Self::HmacSha3($pattern) => $result,
+                }
+            };
+        }
+
+        // Create key using site as the seed
+        let site_key: Vec<u8> = match_hmac!(self, hmac => {
+            hmac.update(seed.as_bytes());
+            hmac.finalize_reset().into_bytes().to_vec()
+        });
+
+        // Create the password, using the byte array to index into the valid password characters
+        let mut password = String::new();
+        for i in site_key {
+            password.push(pass_chars[i as usize % pass_chars.len()]);
+        }
+
+        password
+    }
+}
 
 fn main() -> Result<()> {
+    // Valid password characters
     let pass_chars: Vec<char> =
         "AEIOUaeiouBCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz0123456789!@#$%^&*()"
             .chars()
@@ -18,12 +75,7 @@ fn main() -> Result<()> {
         .read_line(&mut key)
         .expect("Failed to read line");
 
-    // Create master key bytearray from passphrase using scrypt.
-    let master_key = create_master_key(&key)?;
-    debug!("master_key: {:?}", &master_key);
-
-    // Create hmac generator object from the master key.
-    let mut mac = create_hmac(&master_key)?;
+    let mut password_generator = PasswordGenerator::new(PasswordGeneratorAlgo::Sha3, &key)?;
 
     loop {
         // Get site name from user.
@@ -35,34 +87,8 @@ fn main() -> Result<()> {
 
         // Create the site key using the hmac generator and the site name as seed.
         let seed = format!("{}{}", site_name.len(), site_name);
-        let site_key = create_site_key(&mut mac, &seed);
-        debug!("site_key: {:?}", &site_key);
-
-        // Create the password, using the byte array to index into the valid password characters
-        let mut password = String::new();
-        for i in site_key {
-            password.push(pass_chars[i as usize % pass_chars.len()]);
-        }
+        let password = password_generator.create_site_password(&seed, &pass_chars);
 
         println!("{}", password);
     }
-}
-
-fn create_master_key(key: &str) -> Result<[u8; 64]> {
-    let mut master_key: [u8; 64] = [0; 64];
-    let params = scrypt::Params::new(15, 8, 2)?;
-
-    scrypt::scrypt(key.as_bytes(), b"", &params, &mut master_key)?;
-
-    Ok(master_key)
-}
-
-fn create_hmac(key: &[u8]) -> Result<Hmac<Sha512>> {
-    let mac = Hmac::<Sha512>::new_from_slice(&key)?;
-    Ok(mac)
-}
-
-fn create_site_key(mac: &mut Hmac<Sha512>, seed: &str) -> Vec<u8> {
-    mac.update(seed.as_bytes());
-    mac.finalize_reset().into_bytes().to_vec()
 }
